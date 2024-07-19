dc309d0a85da78f6c521e8f5696c6dda08427814
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-07-19 19:34:44 +0545

    Getting the bank and bank account is selected

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ load_dotenv()
 username = os.getenv("USER_NAME")
 password = os.getenv("PASSWORD")
 DPCapital = os.getenv("DPCAPITAL")
+crn= os.getenv("CRN")
+transactionpin= os.getenv("TRANSACTIONPIN")
 
 
 # for login  to the mero share
@@ -40,7 +42,6 @@ passwordEl.send_keys(password)
 
 # Selecting the Login Button and Clicking it
 loginbtn=webdriver.find_elements(By.XPATH,"//button[@type='submit']")[0]
-print(loginbtn)
 loginbtn.click()
 print("Logged in Successfully")
 
@@ -59,15 +60,15 @@ time.sleep(2)
 applyissue = webdriver.find_elements(By.CLASS_NAME,"company-list")
 
 # print(applyissue)
-
+if len(applyissue) == 0:
+    print("No IPO/FPO Available")
+    exit()
 for company in applyissue:
-    print("Comapny Details")
+    print("Getting Comapny Details")
     issuefor = (((company.find_elements(By.XPATH,"//span[@tooltip='Sub Group']")[0]).get_attribute("innerHTML")).strip()).lower()
     issuetype = (((company.find_elements(By.XPATH,"//span[@tooltip='Share Type']")[0]).get_attribute("innerHTML")).strip()).lower()
     sharetype = (((company.find_elements(By.XPATH,"//span[@tooltip='Share Group']")[0]).get_attribute("innerHTML")).strip()).lower()
     button = (company.find_elements(By.XPATH,"//button[@class='btn-issue'][@type='button']")[0])
-    print("Button: ",button)
-    print("Button html : ",button.get_attribute("innerHTML"))
     buttonText = ((button.find_elements(By.TAG_NAME,"i")[0].get_attribute("innerHTML")).strip()).lower()
     print(f"Issue For: {issuefor} \nIssue Type: {issuetype} \nShare Type: {sharetype} \nButton Text: {buttonText} \n")
     # check contains or nto 
@@ -75,23 +76,51 @@ for company in applyissue:
     if "general" and "public" in issuefor and ("ipo" in issuetype or "fpo" in issuetype) and "ordinary" in sharetype and "apply" in buttonText:
     # if "general public" in issuefor and "ipo" in issuetype and "apply" in buttonText:
         print("Entering the Apply Section")
-        print("Applying for the IPO")
         button.click()
         time.sleep(2)
-        # print(webdriver.find_elements(By.XPATH,'html')[0].get_attribute("innerHTML"))
-
-        # with open("apply.html","w") as file:
-        #     file.write(webdriver.page_source)
 
         # Selecting the Bank Account
+        print("Selecting the Bank Account")
         bankaccount = webdriver.find_elements(By.XPATH,"//*[@id='selectBank']/option")[1]
-        print(bankaccount)
         bankaccount.click()
         time.sleep(2)
 
 
         # Selecting the Bank Accoutn Number
+        print("Selecting the Bank Account Number")
         bankaccountnumber = webdriver.find_elements(By.XPATH,"//*[@id='accountNumber']/option")[1]
-        print(bankaccountnumber)
-        
+        bankaccountnumber.click()
+        time.sleep(2)
 
+        # Entering the Quantity to apply
+        print("Entering the Quantity to Apply")
+        unitstoapply=webdriver.find_element(By.ID,'appliedKitta')
+        unitstoapply.send_keys("10")
+
+
+        #Entering the CRN Number
+        print("Entering the CRN Number")
+        crnnumber=webdriver.find_element(By.ID,'crnNumber')
+        crnnumber.send_keys(crn)
+
+        #Checking the Terms and Conditions
+        print("Checking the Terms and Conditions")
+        termsandconditions = webdriver.find_element(By.ID,'disclaimer')
+        termsandconditions.click()
+
+        # Clicking the Proceed Button
+        print("Clicking the Proceed Button")
+        proceedbutton = webdriver.find_element(By.XPATH,"//*[@id='main']/div/app-issue/div/wizard/div/wizard-step[1]/form/div[2]/div/div[5]/div[2]/div/button[1]")
+        proceedbutton.click()
+
+        # Entering the PIN Number 
+        print("Entering the PIN Number")
+        pininput=webdriver.find_element(By.ID,'transactionPIN')
+        pininput.send_keys(transactionpin)
+
+        # Clicking the Apply Button
+        applybutton=webdriver.find_element(By.XPATH,"//*[@id='main']/div/app-issue/div/wizard/div/wizard-step[2]/div[2]/div/form/div[2]/div/div/div/button[1]")
+        # applybutton.click()
+        print("Successfully Applied for the IPO")
+    else:
+        print("No IPO/FPO Available for the Company")
